47cd5f7f94db37d91ad9c243126a5db7dc30ec34
awpillen <awpillen@svsu.edu>
2020-11-02 15:14:24 -0500

    updated #9

--- Assignments/assignment04excercises.js
+++ Assignments/assignment04excercises.js
@@ -137,7 +137,7 @@ appendTableRow4(table3b,"Grand Total",(1.00+2.00+3.00).toFixed(2),1+2+3,((1.00*1
 <!-- code below is from: https://www.guru99.com/practical-code-examples-using-javascript.html -->
 
     // initialize error div id array
-    var divs = new Array();
+    let divs = new Array();
     divs[0] = "errFirst";
     divs[1] = "errLast";
     divs[2] = "errEmail";
@@ -145,10 +145,10 @@ appendTableRow4(table3b,"Grand Total",(1.00+2.00+3.00).toFixed(2),1+2+3,((1.00*1
     divs[4] = "errPassword";
     divs[5] = "errConfirm";
 
-    // function: validate() ---------------------------------------------
-    function validate() {
+// function: validate() ---------------------------------------------
+    function validate(i){
         // initialize input array
-        var inputs = new Array();
+        let inputs = new Array();
         inputs[0] = document.getElementById('first').value;
         inputs[1] = document.getElementById('last').value;
         inputs[2] = document.getElementById('email').value;
@@ -156,7 +156,50 @@ appendTableRow4(table3b,"Grand Total",(1.00+2.00+3.00).toFixed(2),1+2+3,((1.00*1
         inputs[4] = document.getElementById('password').value;
         inputs[5] = document.getElementById('confirm').value;
         // initialize error array
-        var errors = new Array();
+        let errors = new Array();
+        errors[0] = "<span style='color:red'>Please enter your first name!</span>";
+        errors[1] = "<span style='color:red'>Please enter your last name!</span>";
+        errors[2] = "<span style='color:red'>Please enter your email!</span>";
+        errors[3] = "<span style='color:red'>Please enter your user id!</span>";
+        errors[4] = "<span style='color:red'>Please enter your password!</span>";
+        errors[5] = "<span style='color:red'>Please confirm your password!</span>";
+        // update error array with error message
+        let errMessage = errors[i];
+        let div = divs[i];
+        if (inputs[i] == "")
+                document.getElementById(div).innerHTML = errMessage;
+            else if (i == 2) {
+                let atpos = inputs[i].indexOf("@");
+                let dotpos = inputs[i].lastIndexOf(".");
+                if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= inputs[i].length)
+                    document.getElementById('errEmail').innerHTML
+                      = "<span style='color: red'>Enter a valid email address!</span>";
+                else
+                    document.getElementById(div).innerHTML = "OK!";
+            } else if (i == 5) {
+                let first = document.getElementById('password').value;
+                let second = document.getElementById('confirm').value;
+                if (second != first)
+                    document.getElementById('errConfirm').innerHTML
+                      = "<span style='color: red'>Your passwords don't match!</span>";
+                else
+                    document.getElementById(div).innerHTML = "OK!";
+            } else
+                document.getElementById(div).innerHTML = "OK!";
+    }
+
+    // function: validateAll() ---------------------------------------------
+    function validateAll() {
+        // initialize input array
+        let inputs = new Array();
+        inputs[0] = document.getElementById('first').value;
+        inputs[1] = document.getElementById('last').value;
+        inputs[2] = document.getElementById('email').value;
+        inputs[3] = document.getElementById('uid').value;
+        inputs[4] = document.getElementById('password').value;
+        inputs[5] = document.getElementById('confirm').value;
+        // initialize error array
+        let errors = new Array();
         errors[0] = "<span style='color:red'>Please enter your first name!</span>";
         errors[1] = "<span style='color:red'>Please enter your last name!</span>";
         errors[2] = "<span style='color:red'>Please enter your email!</span>";
@@ -165,21 +208,21 @@ appendTableRow4(table3b,"Grand Total",(1.00+2.00+3.00).toFixed(2),1+2+3,((1.00*1
         errors[5] = "<span style='color:red'>Please confirm your password!</span>";
         // update error array with error message
         for (i in inputs) {
-            var errMessage = errors[i];
-            var div = divs[i];
+            let errMessage = errors[i];
+            let div = divs[i];
             if (inputs[i] == "")
                 document.getElementById(div).innerHTML = errMessage;
             else if (i == 2) {
-                var atpos = inputs[i].indexOf("@");
-                var dotpos = inputs[i].lastIndexOf(".");
+                let atpos = inputs[i].indexOf("@");
+                let dotpos = inputs[i].lastIndexOf(".");
                 if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= inputs[i].length)
                     document.getElementById('errEmail').innerHTML
                       = "<span style='color: red'>Enter a valid email address!</span>";
                 else
                     document.getElementById(div).innerHTML = "OK!";
             } else if (i == 5) {
-                var first = document.getElementById('password').value;
-                var second = document.getElementById('confirm').value;
+                let first = document.getElementById('password').value;
+                let second = document.getElementById('confirm').value;
                 if (second != first)
                     document.getElementById('errConfirm').innerHTML
                       = "<span style='color: red'>Your passwords don't match!</span>";
@@ -192,9 +235,9 @@ appendTableRow4(table3b,"Grand Total",(1.00+2.00+3.00).toFixed(2),1+2+3,((1.00*1
 
     // function: finalValidate() ------------------------------------
     function finalValidate() {
-        var count = 0;
+        let count = 0;
         for (i = 0; i < 6; i++) {
-            var div = divs[i];
+            let div = divs[i];
             if (document.getElementById(div).innerHTML == "OK!")
                 count = count + 1;
         }
@@ -202,7 +245,6 @@ appendTableRow4(table3b,"Grand Total",(1.00+2.00+3.00).toFixed(2),1+2+3,((1.00*1
             document.getElementById("errFinal").innerHTML
               = "All the data you entered is correct!!!";
     }
-
 
 // 10. Create a more object-oriented form
 
